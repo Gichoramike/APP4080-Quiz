00d4ce5040f1bec1765fa56ad9d27281806916ab
Gichoramike <mikegichora4@gmail.com>
2023-10-09 13:13:36 +0300

    Added project

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const port =3000;
+const bodyParser = require('body-parser');
+const urlEncodedParser = bodyParser.urlencoded({extended:false});
+
+
+app.set('views','views');
+app.set('view engine','hbs');
+app.use(express.static('public'));
+
+
+app.get('/contacts',function(request,response)
+{
+    response.render('contact_us');
+});
+
+app.post('/process-contacts',urlEncodedParser,function(request,response)
+{
+    response.end('thankyou' + request.body.first_name + ' ' + request.body.last_name);
+
+});
+
+
+app.listen(port);
+console.log('server is listeing on port 3000');
